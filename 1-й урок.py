61f53bd7c36fca16cd494427b061b8a3f65543e1
Balaraphon <Balaraphon@yandex.ru>
2021-02-28 19:30:13 +0300

    Навел красоту

--- 1-й урок.py
+++ 1-й урок.py
@@ -1,11 +1,18 @@
-from decimal import Decimal
-
 def age_in_future():
     birth_date = int(input('В каком году вы родились?: '))
     future_year = int(input('К какому году нужно посчитать возраст?: '))
     age_in_time = future_year - birth_date
 
-    print('Ваш возраст в будущем', age_in_time, "годов")
+    if age_in_time % 10 == 1:
+        print('Ваш возраст в будущем', age_in_time, 'год')
+    elif age_in_time % 10 == 2: 
+        print('Ваш возраст в будущем', age_in_time, 'года')
+    elif age_in_time % 10 == 3:
+        print('Ваш возраст в будущем', age_in_time, 'года')
+    elif age_in_time % 10 == 4:
+        print('Ваш возраст в будущем', age_in_time, 'года')
+    else:
+        print('Ваш возраст в будущем', age_in_time, 'лет')
   
 
 def average_salary():
@@ -13,5 +20,7 @@ def average_salary():
     second_person = int(input('Какая у вас зарплата?: '))
     third_person = int(input('Какая у вас зарплата?: '))
 
-    print('Каждый член семьи может потратить: ', round(first_person + second_person + third_person) / 3)
-   
+    print('Каждый член семьи может потратить: ', round((first_person + second_person + third_person) / 3))
+
+
+  
