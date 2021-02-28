df6e1ed5c2e36f4c8121caa3632cde622e57c50f
Balaraphon <Balaraphon@yandex.ru>
2021-02-28 23:52:26 +0300

    Довел до ума

--- 1-й урок.py
+++ 1-й урок.py
@@ -3,16 +3,35 @@ def age_in_future():
     future_year = int(input('К какому году нужно посчитать возраст?: '))
     age_in_time = future_year - birth_date
 
-    if age_in_time % 10 == 1:
-        print('Ваш возраст в будущем', age_in_time, 'год')
-    elif age_in_time % 10 == 2: 
-        print('Ваш возраст в будущем', age_in_time, 'года')
-    elif age_in_time % 10 == 3:
-        print('Ваш возраст в будущем', age_in_time, 'года')
-    elif age_in_time % 10 == 4:
-        print('Ваш возраст в будущем', age_in_time, 'года')
+    
+    if  len(str(age_in_time))>1 and (str(age_in_time)[-1]=='1' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='2' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='3' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='4' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='5' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='6' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='7' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='8' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif len(str(age_in_time))>1 and (str(age_in_time)[-1]=='9' and str(age_in_time)[-2]=='1'):
+            print('Ваш возраст в будущем',age_in_time,'лет')
+    elif str(age_in_time)[-1]=='1':
+        print('Ваш возраст в будущем',age_in_time,'год')                                       
+    elif str(age_in_time)[-1]=='2':
+        print('Ваш возраст в будущем',age_in_time,'года')
+    elif str(age_in_time)[-1]=='3': 
+        print('Ваш возраст в будущем',age_in_time,'года')
+    elif str(age_in_time)[-1]=='4': 
+        print('Ваш возраст в будущем',age_in_time,'года')
     else:
-        print('Ваш возраст в будущем', age_in_time, 'лет')
+        print('Ваш возраст в будущем',age_in_time,'лет')
   
 
 def average_salary():
