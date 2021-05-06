661cadef13eef917ff01c63c7ea9db4053f67ab0
Balaraphon <Balaraphon@yandex.ru>
2021-05-06 18:07:58 +0300

    Добавил функцию смотреть прогноз на несколько дней

--- TeleBot/myproekt.py
+++ TeleBot/myproekt.py
@@ -2,6 +2,10 @@ import random
 import telebot
 import requests
 from bs4 import BeautifulSoup as BS
+from telebot import types
+import re
+import datetime
+import time
 
 
 bot = telebot.TeleBot("1777037080:AAGHut45GniubiW4f5ejHqbD0RYeMz1We2w")
@@ -13,33 +17,84 @@ soup = BS(data, 'html.parser')
 list_of_Russia_city = []
 links = soup.find_all('tr')
 for link in links:
-    link = (link.find('td', {'align':'left'}))
+    link = (link.find('td', {'align': 'left'}))
     if link != None and link.has_attr('align'):
         list_of_Russia_city.append(link.find('a').text)
 
 def pars_weather(city):
-    r = requests.get(f'https://sinoptik.ua/погода-{city}')
-    html = BS(r.content, 'html.parser')
-    for el in html.select('#content'):
-        tem_min = el.select('.temperature .min')[0].text
-        tem_max = el.select('.temperature .max')[0].text
-        text = el.select('.wDescription .description')[0].text
-        return f'{tem_min}, {tem_max}.{text}'
+    date = datetime.datetime.today()
+    date = datetime.datetime.date(date)
+    print(date)
+    tomorrow = datetime.datetime.today()
+    tomorrow = tomorrow + datetime.timedelta(days=1)
+    tomorrow = datetime.datetime.date(tomorrow)
+    print(tomorrow)
+    if re.search(r'\/', city):
+        citys = re.sub(r'[а-я-А-Я]+\/', '', city)
+        citys = citys.replace('-', '')
+        citys = datetime.datetime.strptime(citys, '%Y%m%d').date()
+        if citys > date and citys == tomorrow:
+            r = requests.get(f'https://sinoptik.ua/погода-{city}')
+            html = BS(r.content, 'html.parser')
+            for el in html.select('#content'):
+                zavtra = 'Завтра ' + el.select('.temperature .min')[1].text, el.select('.temperature .max')[1].text
+                text = el.select('.wDescription .description')[0].text
+                return f'{str(zavtra[0])} {str(zavtra[1])}. {text}'
+        elif citys > tomorrow:
+            r = requests.get(f'https://sinoptik.ua/погода-{city}')
+            html = BS(r.content, 'html.parser')
+            for el in html.select('#content'):
+                poslezavtra = 'Послезавтра ' + el.select('.temperature .min')[2].text, el.select('.temperature .max')[2].text
+                text = el.select('.wDescription .description')[0].text
+                return f'{str(poslezavtra[0])} {str(poslezavtra[1])}. {text}'
+        else:
+            pass
+    else:
+        r = requests.get(f'https://sinoptik.ua/погода-{city}')
+        html = BS(r.content, 'html.parser')
+        for el in html.select('#content'):
+            tem_min = el.select('.temperature .min')[0].text
+            tem_max = el.select('.temperature .max')[0].text
+            text = el.select('.wDescription .description')[0].text
+            return f'{tem_min}, {tem_max}.{text}'
 
 @bot.message_handler(content_types = ['text'])
 def send_welcome(message):
     global list_of_Russia_city
-    intro = random.choice(['Привет, ', 'Добрый день! ', 'Отличный Вопрос! '])
+    intro = random.choice(['Гугл говорит: ', 'Давай глянем! ', 'Отличный Вопрос! ', 'Мне тоже интересно! '])
     city = message.text
     if city == '/start':
         pass
     else:
         city = city.title().strip()
-    if city in list_of_Russia_city:
-        bot.send_message(message.chat.id, f'{intro}{message.from_user.first_name}, погода в городе {city} сегодня:\n' + str(pars_weather(city)))
-    elif city == '/start':
-        bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}. Я Бот который показывает погоду в городах России. Для моей корректной работы напиши, пожалуйста, название города России')
+    if re.search(r'[а-я-А-Я]+\s[скоро]+', message.text):
+        if re.search(r'\s[скоро]+', message.text).group(0).strip().lower() == 'скоро'.lower() and re.search(r'[а-я-А-Я]+', message.text).group(0).strip() in list_of_Russia_city:
+            city = re.sub(r'\s[а-я-А-Я]+', '', message.text)
+            city = city.title().strip()
+            date = datetime.datetime.today()
+            date = datetime.datetime.date(date)
+            tomorrow = datetime.datetime.today()
+            tomorrow = tomorrow + datetime.timedelta(days=1)
+            tomorrow = tomorrow.strftime("%Y-%m-%d")
+            tomorrow = city + '/' + tomorrow
+            print(tomorrow+'go')
+            day_after_tomorrow = datetime.datetime.today()
+            day_after_tomorrow = day_after_tomorrow + datetime.timedelta(days=2)
+            day_after_tomorrow = day_after_tomorrow.strftime("%Y-%m-%d")
+            day_after_tomorrow = city + '/' + day_after_tomorrow
+            print(day_after_tomorrow+'gogo')
+            bot.send_message(message.chat.id, f'{intro}{message.from_user.first_name}, погода в городе {city} на ближайшие дни:\n \n' + f'Сегодня: {date} ' + str(pars_weather(city)) +'\n' \
+                             + '\n' + str(pars_weather(tomorrow)) + '\n' +'\n' + str(pars_weather(day_after_tomorrow)))
+        else:
+            bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}. Если хочешь узнать погоду на несколько дней, напиши название города, поставь пробел и добавь слово "скоро"')
     else:
-        bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}. В России нет такого города, введи, пожалуйста, корректное название города')
+        if city in list_of_Russia_city:
+            bot.send_message(message.chat.id, f'{intro}{message.from_user.first_name}, погода в городе {city} сегодня:\n' + str(pars_weather(city)))
+        elif city == '/start':
+            bot.send_message(message.chat.id, f'Привет, {message.from_user.first_name}. Я - Бот, который показывает погоду в городах России.\n Если хочешь узнать погоду на несколько дней, напиши название города, поставь пробел и добавь слово "скоро".\n Если хочешь узнать погоду только на сегодня, напиши только название города. Погода в каком городе России интересует тебя?')
+        elif re.search(r'[а-я-А-Я]+\s\/[a-z]+', city):
+            pass
+        else:
+            bot.send_message(message.chat.id, f'Привет {message.from_user.first_name}. В России нет такого города, введи, пожалуйста, корректное название города')
         
 bot.polling()
